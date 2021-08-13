f85b5c295d333d28300f34fc66fe8ac42aa52cac
unknown <btukenalieva@gmail.com>
2021-08-13 16:17:49 +0600

    added choices to Category

--- products/models.py
+++ products/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.db.models.enums import TextChoices
+from django.db.models.fields import SlugField
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=250,
+    choices=(
+        ("Одеж", "Oдежда"),
+        ("Трант", "Транспорт"),
+        ("Усл", "Услуги"),
+        ("Игруш", "Игрушки"),
+        ("Меб", "Мебель"),
+        ("Элект", "Электроника"),
+
+    ))
+    slug = models.SlugField(max_length=250, unique=True, blank=True)
+
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+    name = models.CharField(max_length=250, db_index=True)
+    slug = models.SlugField(max_length=250, db_index=True, blank=True)
+    description = models.TextField()
+    price = models.DecimalField(blank=True, decimal_places=2, max_digits=12)
+    image = models.ImageField(blank=True, null=True, upload_to='products_photo')
+    status = models.CharField(max_length=1, choices=(
+        ("П", "Продать"),
+        ("К", "Купить"),
+    ))
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('name',)
+        index_together = (('id', 'slug'),)
+
+    def __str__(self):
+        return self.name 
--- products/views.py
+++ products/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
+from .models import Category, Product
 
-# Create your views here.
+
