dbbbd47f0b48ef9928d825cb254e41c8b5e27bbf
unknown <btukenalieva@gmail.com>
2021-08-13 23:02:29 +0600

    added class in admin.py for products

--- products/admin.py
+++ products/admin.py
@@ -2,6 +2,17 @@ from django.contrib import admin
 from .models import Category, Product
 
 # class AdminProduct(admin.ModelAdmin):
-admin.site.register(Category)
-admin.site.register(Product)
 
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug':('name',)}
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['category', 'name', 'description', 'status', 'price', 'created', 'updated']
+    list_filter = ['category', 'status']
+    list_editable = ['price', 'status']
+    prepopulated_fields = {'slug':('name',)}
