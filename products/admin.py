0b8d461a3ee09c3a176005b9cea557545ec843a3
Bermet <74715167+Bermet1@users.noreply.github.com>
2021-09-03 17:21:11 +0600

    Update admin.py

--- products/admin.py
+++ products/admin.py
@@ -1,30 +1,39 @@
 from django.contrib import admin
 from .models import Category, Product, FeedBack
 
-
-# class AdminProduct(admin.ModelAdmin):
-
+class SubcategoryInline(admin.TabularInline):
+    model = Subcategory
+    extra = 3
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ['name', 'slug']
-    prepopulated_fields = {'slug':('name',)}
+    list_display = ['name']
+    fieldsets = [
+        (
+            None, {
+                'fields':('name',)
+            }
+        )
+    ]
+    inlines = (SubcategoryInline,)
+
+@admin.register(Subcategory)
+class SubategoryAdmin(admin.ModelAdmin):
+    list_display = ['name']
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['category', 'name', 'description', 'status', 'price', 'created', 'updated']
+    list_display = ['category', 'sub_category', 'name', 'description', 'status', 'price', 'created', 'updated']
     list_filter = ['category', 'status']
-    list_editable = ['price', 'status']
-    prepopulated_fields = {'slug':('name',)}
 
 
 @admin.register(FeedBack)
 class FeedbackAdmin(admin.ModelAdmin):
     list_display = ['text', 'product', 'user']
     search_fields = ['text', 'product']
-    fields = ['user', 'product', 'text']
-    readonly_fields = ['product', 'text'] 
+    list_filter = ['created', 'updated', 'product']
+ 
     
     
 
